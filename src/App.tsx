c4dcf008351ed5659dd581e7678f19b769495bfa
khayitmuradov <khayitmuradov@proton.me>
2023-04-01 12:14:58 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='font-bold text-[30px] text-center'>Starting typescript react transferring logistic products with ant design using Tailwind css !</h1>
+      <Button type="primary">Click me!</Button>
     </div>
   );
 }
