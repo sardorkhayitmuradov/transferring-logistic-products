9ad7af65656eb409751c30c1f0bfd14433b85397
khayitmuradov <khayitmuradov@proton.me>
2023-04-01 13:01:24 +0500

    dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Button } from 'antd';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='font-bold text-[30px] text-center'>Starting typescript react transferring logistic products with ant design using Tailwind css !</h1>
-      <Button type="primary">Click me!</Button>
+    <div className="h-full">
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import "./index.css";
+
+const { Header, Sider, Content } = Layout;
+
+const Home: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{ height: "100%" }}>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        breakpoint="lg"
+        collapsedWidth="0"
+        style={{ height: "100vh" }}
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="text-white text-center text-[20px] h-[32px] m-4 bg-[#ffffff4d]">
+          Logo
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "Dashboard",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "Clients",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "Products",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout" style={{ height: "100vh" }}>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Home;
